83efd6fbf235ec1364e1bfcd8a20baa164b3dada
Rogelio Alvarado Vilchis <rogalvil@gmail.com>
2024-06-18 16:24:00 -0600

    Setup action policy on action controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,15 @@
 
 # Application controller is the parent class of all controllers in Rails.
 class ApplicationController < ActionController::API
+  include ActionPolicy::Controller
+  authorize :user, through: :current_user
+
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  rescue_from ActionPolicy::Unauthorized do |ex|
+    render json: { message: "User not allowed to #{ex.rule}" }, status: :forbidden
+  end
 
   protected
 
