989e5a168c5d17c5a1bd9d9fe532421f36eb45ca
Rogelio Alvarado Vilchis <rogalvil@gmail.com>
2024-06-18 22:14:07 -0600

    Add rescu for record not found and routing error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,11 +12,26 @@ class ApplicationController < ActionController::API
     render json: { message: "User not allowed to #{ex.rule}" }, status: :forbidden
   end
 
+  rescue_from ActiveRecord::RecordNotFound do
+    render json: { message: 'Record not found' }, status: :not_found
+  end
+
+  rescue_from ActionController::RoutingError, with: :render_not_found
+
+  def route_not_found
+    raise ActionController::RoutingError, 'Not Found'
+  end
+
+  private
+
+  def render_not_found
+    render json: { messages: ['The requested route was not found'] }, status: :not_found
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
-
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name])
   end
 end
