d06d48f38fb46dc400ffbdcead68d97db046aa8b
Rogelio Alvarado Vilchis <rogalvil@gmail.com>
2024-06-19 02:50:17 -0600

    Remove duplciate methods and add relation scope

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -2,15 +2,24 @@
 
 # UserPolicy class
 class UserPolicy < ApplicationPolicy
-  def index?
-    user.admin?
-  end
-
-  def customers?
-    true
-  end
+  ERROR_MESSAGES = {
+    update?: 'You are not allowed to update this user',
+    show?: 'You are not allowed to view this user\'s information'
+  }.freeze
 
   def show?
     user.admin? || record.customer?
+  end
+
+  relation_scope do |relation|
+    next relation if user.admin?
+
+    relation.where(role: 'customer')
+  end
+
+  private
+
+  def owner?
+    record == user
   end
 end
